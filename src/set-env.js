93ff402410e35c20cba9662159335d349112fbb8
Jilu <mayday23.manalo@gmail.com>
2026-03-30 20:28:30 +0800

    Updated environment variables

---
+++ src/set-env.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const content = `
+export const environment = {
+  production: true,
+  apiUrl: '${process.env.NG_APP_GOOGLE_API_KEY || ''}'
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.ts', content);
+console.log('environment.ts generated successfully');
